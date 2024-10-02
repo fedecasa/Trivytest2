8103b69dd3e0cfad102237c2f9abde10788f3434
fedecasa <145713797+fedecasa@users.noreply.github.com>
2024-10-02 08:48:50 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
-int main(){
-    char last_name[20];
-    printf ("Enter your last name: ");
-    scanf ("%s", last_name);
-    return 0;
-}
+// Encoding misses single quote allowing XSS with payload (e.g. '+alert(1)+')
+<script>
+    <%
+        String searchTxt = StringEscapeUtils.escapeHtml4(request.getParameter("search"));
+    %>
+
+    document.cookie = 'search=<%=searchTxt%>';
+</script>
